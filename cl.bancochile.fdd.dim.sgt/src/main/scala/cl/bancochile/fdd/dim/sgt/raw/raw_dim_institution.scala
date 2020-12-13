f78453b8e22f92be697e9ae434c756faf63a4d9c
TCS <raul.ibarra@tcs.com>
2020-12-13 16:46:19 +0000

    ADD: Dimension table tbl_dim_institution

---
+++ cl.bancochile.fdd.dim.sgt/src/main/scala/cl/bancochile/fdd/dim/sgt/raw/raw_dim_institution.scala
@@ -0,0 +1,179 @@
+package cl.bancochile.fdd.dim.sgt.raw 
+         
+//Project's global setting 
+import cl.bancochile.fdd.todos.globalSettings._ 
+ 
+import com.huemulsolutions.bigdata.common._ 
+import com.huemulsolutions.bigdata.control._ 
+import com.huemulsolutions.bigdata.datalake._ 
+import com.huemulsolutions.bigdata.tables._ 
+import org.apache.spark.sql.types._ 
+import com.huemulsolutions.bigdata.control.huemulType_Frequency._ 
+ 
+ 
+/** 
+ * Clase que permite abrir un archivo de texto, devuelve un objeto huemul_dataLake con un DataFrame de los datos 
+ * ejemplo de nombre: raw_institucion_mes 
+ */ 
+class raw_dim_institution(huemulBigDataGov: huemul_BigDataGovernance, Control: huemul_Control) extends huemul_DataLake(huemulBigDataGov, Control) with Serializable  { 
+   this.Description = "Instituciones" 
+   this.GroupName = ("cmf").toUpperCase() 
+    
+   //Crea variable para configuracion de lectura del archivo 
+   val CurrentSetting = new huemul_DataLakeSetting(huemulBigDataGov) 
+ 
+   //setea la fecha de vigencia de esta configuracion 
+   CurrentSetting.StartDate = huemulBigDataGov.setDateTime(2019,1,1,0,0,0) 
+   CurrentSetting.EndDate = huemulBigDataGov.setDateTime(2021,2,1,0,0,0) 
+    
+   //Configuracion de rutas globales 
+   CurrentSetting.GlobalPath = huemulBigDataGov.GlobalSettings.RAW_SmallFiles_Path 
+ 
+   //Configura ruta local, se pueden usar comodines 
+   CurrentSetting.LocalPath = "sgt/" 
+ 
+   //configura el nombre del archivo (se pueden usar comodines) 
+   CurrentSetting.FileName = "GDD_M_SGT_Instituciones_{{YYYY}}{{MM}}.dat" 
+ 
+   //especifica el tipo de archivo a leer 
+   CurrentSetting.FileType = huemulType_FileType.TEXT_FILE 
+ 
+   //expecifica el nombre del contacto del archivo en TI 
+   CurrentSetting.ContactName = "Herman Sotomayor" 
+ 
+   //Indica como se lee el archivo 
+   CurrentSetting.DataSchemaConf.ColSeparatorType = huemulType_Separator.CHARACTER  //POSITION;CHARACTER 
+    
+   //separador de columnas 
+   CurrentSetting.DataSchemaConf.ColSeparator = "\\|\\|"    //SET FOR CARACTER 
+   
+    
+   //========================================================= 
+   // Auto-Gen columns information 
+   //========================================================= 
+ 
+ 
+     
+ 
+  //--demo 
+ 
+  
+CurrentSetting.DataSchemaConf.AddColumns("Id_institucion","Id_institucion",IntegerType,"Id_institucion")
+
+CurrentSetting.DataSchemaConf.AddColumns("Nombre_institucion","Nombre_institucion",StringType,"Nombre_institucion")
+
+
+
+ 
+  //========================================================== 
+  // Auto-Gen Log schema if applies 
+  // ========================================================= 
+ 
+   CurrentSetting.LogSchemaConf.ColSeparatorType = huemulType_Separator.CHARACTER 
+   CurrentSetting.LogNumRows_FieldName = null 
+   CurrentSetting.LogSchemaConf.ColSeparator = "\\|\\|"  
+   CurrentSetting.LogSchemaConf.setHeaderColumnsString( "VACIO" ) 
+    
+ 
+  //--Cabecera 
+   
+    CurrentSetting.LogSchemaConf.AddColumns("Cabecera negocio","Cabecera logico",StringType,"Es una cabecera",0,0) 
+   
+   
+  //--Pie 
+   
+ 
+	 
+   //apply configuration 
+   this.SettingByDate.append(CurrentSetting) 
+    
+      
+    /*** 
+   * open(ano: Int, mes: Int) <br> 
+   * metodo que retorna una estructura con un DF de detalle, y registros de control <br> 
+   * ano: anio de los archivos recibidos <br> 
+   * mes: mes de los archivos recibidos <br> 
+   * dia: dia de los archivos recibidos <br> 
+   * Retorna: true si todo esta OK, false si tuvo algun problema <br> 
+  */ 
+  def open(Alias: String, ControlParent: huemul_Control, ano: Integer, mes: Integer, dia: Integer, hora: Integer, min: Integer, seg: Integer): Boolean = { 
+    //Crea registro de control de procesos 
+     val control = new huemul_Control(huemulBigDataGov, ControlParent, huemulType_Frequency.ANY_MOMENT) 
+      
+    //Guarda los parametros importantes en el control de procesos 
+    control.AddParamYear("Ano", ano) 
+    control.AddParamMonth("Mes", mes) 
+    control.AddParamMonth("Dia", dia) 
+       
+    try {  
+      //NewStep va registrando los pasos de este proceso, tambien sirve como documentacion del mismo. 
+      control.NewStep("Abre archivo RDD y devuelve esquemas para transformar a DF") 
+ 
+      if (!this.OpenFile(ano, mes, dia, hora, min, seg, null)){ 
+        //Control tambien entrega mecanismos de envio de excepciones 
+        control.RaiseError(s"Error al abrir archivo: ${this.Error.ControlError_Message}") 
+      } 
+    
+      control.NewStep("Aplicando Filtro") 
+      //Si el archivo no tiene cabecera, comentar la linea de .filter 
+ 
+      val rowRDD = this.DataRDD 
+            //filtro para considerar solo las filas que los dos primeros caracteres son numericos 
+            .filter { x => x.length()>=4 && huemulBigDataGov.isAllDigits(x.substring(0, 1) )  } 
+ 
+            //filtro para dejar fuera la primera fila 
+            .filter { x => x != this.Log.DataFirstRow  } 
+            .map { x => this.ConvertSchema(x) } 
+             
+      control.NewStep("Transformando datos a dataframe")       
+ 
+      //Crea DataFrame en Data.DataDF 
+      this.DF_from_RAW(rowRDD, Alias) 
+                           
+      control.FinishProcessOK                       
+    } catch { 
+      case e: Exception => { 
+        control.Control_Error.GetError(e, this.getClass.getName, null) 
+        control.FinishProcessError()    
+      } 
+    }          
+    return control.Control_Error.IsOK() 
+  } 
+} 
+ 
+ 
+/** 
+ * Este objeto se utiliza solo para probar la lectura del archivo RAW 
+ * La clase que esta definida mas abajo se utiliza para la lectura. 
+ */ 
+object raw_dim_institution_test { 
+/** 
+   * El proceso main es invocado cuando se ejecuta este codigo 
+   * Permite probar la configuracion del archivo RAW 
+   */ 
+   
+  def main(args : Array[String]) { 
+     
+    //Creacion API 
+    val huemulBigDataGov  = new huemul_BigDataGovernance(s"Testing DataLake - ${this.getClass.getSimpleName}", args, Global) 
+    //Creacion del objeto control, por default no permite ejecuciones en paralelo del mismo objeto (corre en modo SINGLETON) 
+    val Control = new huemul_Control(huemulBigDataGov, null, huemulType_Frequency.MONTHLY) 
+     
+    /*************** PARAMETROS **********************/ 
+    var param_ano = huemulBigDataGov.arguments.GetValue("year", null, "Debe especificar el parametro anio, ej: ano=2017").toInt 
+    var param_mes = huemulBigDataGov.arguments.GetValue("month", null, "Debe especificar el parametro mes, ej: mes=12").toInt 
+     
+    //Inicializa clase RAW   
+    val DF_RAW =  new raw_dim_institution(huemulBigDataGov, Control) 
+    if (!DF_RAW.open("DF_RAW", null, param_ano, param_mes, 0, 0, 0, 0)) { 
+      println("************************************************************") 
+      println("**********  E  R R O R   E N   P R O C E S O   *************") 
+      println("************************************************************") 
+    } else 
+      DF_RAW.DataFramehuemul.DataFrame.show() 
+       
+    Control.FinishProcessOK 
+    
+  }   
+} 
+
---
+++ cl.bancochile.fdd.dim.sgt/src/main/scala/cl/bancochile/fdd/tables/dim/tbl_dim_institution.scala
@@ -0,0 +1,75 @@
+package cl.bancochile.fdd.tables.dim 
+ 
+import com.huemulsolutions.bigdata.common._ 
+import com.huemulsolutions.bigdata.control._ 
+import com.huemulsolutions.bigdata.tables._ 
+import com.huemulsolutions.bigdata.dataquality._ 
+import org.apache.spark.sql.types._ 
+ 
+ 
+class tbl_dim_institution (huemulBigDataGov: huemul_BigDataGovernance, Control: huemul_Control) extends huemul_Table(huemulBigDataGov, Control) with Serializable { 
+  /**********   C O N F I G U R A C I O N   D E   L A   T A B L A   ****************************************/ 
+  //Tipo de tabla, Master y Reference son catalogos sin particiones de periodo 
+  this.setTableType(huemulType_Tables.Master) 
+ 
+  //Base de Datos en HIVE donde sera creada la tabla 
+  this.setDataBase(huemulBigDataGov.GlobalSettings.DIM_DataBase) 
+ 
+  //Tipo de archivo que sera almacenado en HDFS 
+  this.setStorageType(huemulType_StorageType.PARQUET) 
+ 
+  //Ruta en HDFS donde se guardara el archivo PARQUET 
+  this.setGlobalPaths(huemulBigDataGov.GlobalSettings.MASTER_BigFiles_Path) 
+ 
+  //Ruta en HDFS especifica para esta tabla (Globalpaths / localPath) 
+  this.setLocalPath("dim/") 
+ 
+  //Frecuencia de actualizacion 
+  this.setFrequency(huemulType_Frequency.NOT_SPECIFIED) 
+   
+  /**********   S E T E O   I N F O R M A T I V O   ****************************************/ 
+  //Descripcion de la fuente 
+  this.setDescription("") 
+ 
+  //Nombre del contacto de negocio  
+  this.setBusiness_ResponsibleName("") 
+ 
+  //Nombre del contacto de TI 
+  this.setIT_ResponsibleName("") 
+    
+  
+  /**********   S E G U R I D A D   ****************************************/ 
+  //Solo estos package y clases pueden ejecutar en modo full, si no se especifica todos pueden invocar 
+//  this.WhoCanRun_executeFull_addAccess("process_dim_cuentan_contables", 
+//                                       "cl.bancochile.fdd.dim.cmf.process") 
+  //Solo estos package y clases pueden ejecutar en modo solo Insert, si no se especifica todos pueden invocar 
+//  this.WhoCanRun_executeOnlyInsert_addAccess("process_dim_cuentan_contables", 
+//                                        "cl.bancochile.fdd.dim.cmf.process") 
+  //Solo estos package y clases pueden ejecutar en modo solo Update, si no se especifica todos pueden invocar 
+//  this.WhoCanRun_executeOnlyUpdate_addAccess("process_dim_cuentan_contables", 
+//                                        "cl.bancochile.fdd.dim.cmf.process") 
+   
+/**********   Columns Information   ****************************************/ 
+  //If table is Transaction. period must be create 
+    
+ 
+   
+    val Id_institucion = new huemul_Columns (IntegerType,true,"") 
+    Id_institucion.setNullable(false) 
+    Id_institucion.setIsPK(true) 
+   
+    val Nombre_institucion = new huemul_Columns (StringType,true,"") 
+    Nombre_institucion.setNullable(true) 
+    Nombre_institucion.setIsPK(false) 
+   
+   
+   
+   
+   
+  
+    
+  //-**********Ejemplo para aplicar DataQuality de Integridad Referencial 
+ 
+     
+  this.ApplyTableDefinition() 
+} 
